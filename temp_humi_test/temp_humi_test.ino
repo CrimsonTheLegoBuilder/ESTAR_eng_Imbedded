df0c9413a79ff168568277c19808763a3d814004
crimson <redcube231@gmail.com>
2023-12-15 17:31:04 +0900

    2312151531

--- temp_humi_test/temp_humi_test.ino
+++ temp_humi_test/temp_humi_test.ino
@@ -3,7 +3,8 @@
 #define DHTPIN 7
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
-int RelayPin4 = 4; 
+int RelayPin3 = 3;
+int RelayPin4 = 4;
   /*불러온 라이브러리 안에 몇번 PIN에서 데이터값이 나오는지
   설정해줘야 합니다. 아날로그 0번 PIN인 A0으로 설정했습니다.
   */
@@ -12,7 +13,9 @@ void setup() {
   /*온습도값을 PC모니터로 확인하기위해
   시리얼 통신을 설정해 줍니다. */
   dht.begin();
+  pinMode(RelayPin3, OUTPUT);
   pinMode(RelayPin4, OUTPUT);
+  digitalWrite(RelayPin3, HIGH);
   digitalWrite(RelayPin4, HIGH);
 }
 
@@ -50,5 +53,6 @@ void loop() {
 
   if (h > 50.0) digitalWrite(RelayPin4, LOW);
   else digitalWrite(RelayPin4, HIGH);
+  if (h > 60.0) digitalWrite(RelayPin3, LOW);
+  else digitalWrite(RelayPin3, HIGH);
 }
-
