7c7c0f437a93a6dbac42fe3ab2ff158e00b35ec4
crimson <redcube231@gmail.com>
2024-09-06 17:34:05 +0900

    ,

--- waterheater_test_v01/pinConfig.h
+++ waterheater_test_v01/pinConfig.h
@@ -1,5 +1,14 @@
-#ifndef _SSR_H_
-#define _SSR_H_
+#ifndef _PINCONFIG_H_
+#define _PINCONFIG_H_
 
+#define BTN1_PIN 36
+#define BTN2_PIN 39
+#define BTN3_PIN 34
+#define BTN4_PIN 35
+#define PHOTO_PIN 32
+#define LEVEL_PIN 33
+#define PRESSURE_PIN 25
+#define MOTOR 26
+#define PRESSURE_PIN 25
 
 #endif
