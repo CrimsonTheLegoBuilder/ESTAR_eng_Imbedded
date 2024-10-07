c8521b0fa4befaa23fa6913e18e05b26c5b9a455
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-10-07 16:21:43 +0900

    fuck

--- airpump_test_v01/airpump_test_v01.ino
+++ airpump_test_v01/airpump_test_v01.ino
@@ -21,7 +21,7 @@ void setup() {
   air = 1;
   sol1 = 0;
   sol2 = 0;
-  high = 350;
+  high = 700;
   low = 300;
   digitalWrite(AIRPUMP, air);
   digitalWrite(SOLVALVE1, sol1);
@@ -34,15 +34,15 @@ void loop() {
   if (Serial.available()) {
     int x = Serial.parseInt();
     switch (x) {
-      case 0:
-        force_shut = 1;
-        air = 1;
-        sol1 = 1;
-        sol2 = 1;
-        digitalWrite(AIRPUMP, 1);
-        digitalWrite(SOLVALVE1, sol1);
-        digitalWrite(SOLVALVE2, sol2);
-        break;
+      // case 0:
+      //   force_shut = 1;
+      //   air = 1;
+      //   sol1 = 1;
+      //   sol2 = 1;
+      //   digitalWrite(AIRPUMP, 1);
+      //   digitalWrite(SOLVALVE1, sol1);
+      //   digitalWrite(SOLVALVE2, sol2);
+      //   break;
       case 1:
         sol1 ^= 1;
         digitalWrite(SOLVALVE1, sol1);
@@ -64,7 +64,7 @@ void loop() {
         digitalWrite(SOLVALVE2, sol2);
         break;
       case 5:
-        if (force_shut) force_shut = 0;
+        if (!force_shut) force_shut = 1;
         air = 0;
         digitalWrite(AIRPUMP, air);
         break;
@@ -72,14 +72,17 @@ void loop() {
         air = 1;
         digitalWrite(AIRPUMP, air);
         break;
+      case 9:
+        force_shut = 0;
+        break;
       default:
         break;
     }
   }
-  if (force_shut) {
-    delay(1000);
-    return;
-  }
+  // if (force_shut) {
+  //   delay(1000);
+  //   return;
+  // }
   Serial.print("air: ");
   Serial.print(air);
   Serial.print(" sol1: ");
@@ -89,15 +92,19 @@ void loop() {
   int V = analogRead(PRESSURE_SENSOR);
   Serial.print("voltage: ");
   Serial.println(V);
-  float prs = V * 1.1911 - 276.68;
+  // float prs = V * 1.1911 - 276.68;
+  float prs = V * 0.01106 - 1.3226;
+  int press = map(V, 210, 1024, 100, 1000);
   Serial.print("pressure: ");
-  Serial.println(prs);
-  int f = pressure_check(prs, high, low);
-  if (f == 1) {
-    air = 0;
+  Serial.print(press);
+  Serial.print(" ");
+  Serial.println(press / 100.);
+  int f = pressure_check(V, high, low);
+  if (!force_shut && f == 1) {
+    air = 1;
   }
   else if (f == -1) {
-    air = 1;
+    air = 0;
   }
   //else do nothing;
   digitalWrite(AIRPUMP, air);
